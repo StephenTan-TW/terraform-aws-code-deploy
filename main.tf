e91eecbe24c3f914b3e4f72b85c8f63a8c85aba0
Stephen Tan <stephen.tan@transferwise.com>
2021-10-07 19:37:36 +0100

    Remove dud value interpolation

--- main.tf
+++ main.tf
@@ -176,9 +176,9 @@ resource "aws_codedeploy_deployment_group" "default" {
 
     content {
       ec2_tag_filter {
-        key   = lookup(ec2_tag_filter.value, "key", null)
-        type  = lookup(ec2_tag_filter.value, "type", null)
-        value = lookup(ec2_tag_filter.value, "value", null)
+        key   = lookup(ec2_tag_filter, "key", null)
+        type  = lookup(ec2_tag_filter, "type", null)
+        value = lookup(ec2_tag_filter, "value", null)
       }
     }
   }
